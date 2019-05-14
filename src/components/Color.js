8227d06f6ee05899f04d66573cb2fb3bf4b96978
Tommy Tran <tranttommy95@gmail.com>
2019-05-14 09:44:39 -0700

    made simple color

---
+++ src/components/Color.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Color() {
+  return (
+    <dl>
+      <dt>Name</dt>
+      <dd>Red</dd>
+
+      <dt>Hex</dt>
+      <dd>#FF0000</dd>
+
+      <dt>RGB</dt>
+      <dd>Red - 255</dd>
+      <dd>Green - 0</dd>
+      <dd>Blue - 0</dd>
+    </dl>
+  );
+}
